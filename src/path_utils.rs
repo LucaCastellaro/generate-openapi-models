60bc43bc2164e287aa521216c34aa39a12ac877c
Luca Castellaro <luca.castellaro.lc@gmail.com>
2023-11-02 10:08:59 +0100

    Create destination dir if it doesn't exist

--- src/path_utils.rs
+++ src/path_utils.rs
@@ -1,9 +1,11 @@
-use std::path::{Path, PathBuf};
+use std::{path::{Path, PathBuf}, fs};
 
-pub fn is_path_valid(project_path: &str) -> Result<PathBuf, String> {
+use crate::types;
+
+pub fn is_path_valid(project_path: &str) -> Result<PathBuf, types::DynError> {
     let path = Path::new(project_path);
     if !Path::exists(path) {
-        return Err(format!("Percorso non valido: {:#?}", path));
+        fs::create_dir(path)?;
     }
     return Ok(path.to_path_buf());
 }
